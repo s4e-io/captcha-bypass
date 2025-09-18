c109c19b774fbe7c1be86fb1a20d9649c2ff7574
Kayra Öksüz <oksuzkayra16@gmail.com>
2025-09-18 10:52:35 +0300

    add header lower

--- core/bypass.py
+++ core/bypass.py
@@ -10,14 +10,14 @@ def try_bypass(url: str, req: str, token = None):
         bypass_list[2] = methods_get.bypass_method_3_get(url, req, token)
         bypass_list[3] = methods_get.bypass_method_4_post(url, req, token)
         bypass_list[4] = methods_get.bypass_method_4_put(url, req, token)
-    elif "Content-Type: application/json" in req:
+    elif "content-type: application/json" in req.lower():
         bypass_list[0] = methods_json.bypass_method_1_json(url, req, token)
         bypass_list[1] = methods_json.bypass_method_2_json(url, req, token)
         bypass_list[2] = methods_json.bypass_method_3_json(url, req, token)
         bypass_list[3] = methods_json.bypass_method_4_json_get(url, req, token)
         bypass_list[4] = methods_json.bypass_method_4_json_put(url, req, token)
 
-    elif "Content-Type: application/x-www-form-urlencoded" in req:
+    elif "content-type: application/x-www-form-urlencoded" in req.lower():
         bypass_list[0] = methods_urlencoded.bypass_method_1_urlencoded(url, req, token)
         bypass_list[1] = methods_urlencoded.bypass_method_2_urlencoded(url, req, token)
         bypass_list[2] = methods_urlencoded.bypass_method_3_urlencoded(url, req, token)
